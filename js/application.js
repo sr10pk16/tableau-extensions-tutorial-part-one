c845db437eaf5ce96e6d9e61cafbfb07032f6084
sr10pk16 <pk_prashant@yahoo.com>
2020-05-03 04:11:33 +0530

    1

--- js/application.js
+++ js/application.js
@@ -3,19 +3,16 @@
 (function () {
   $(document).ready(function () {
     // Initialises Tableau Data Extension
-
-    document.write("Hello");
     tableau.extensions.initializeAsync().then(function () {
-      refresh();
+        refresh();
     }, function () { console.log('Error while Initializing: ' + err.toString()); });
   });
  
   function refresh() {
     // Gets a list of the worksheets and adds them to the web page.
-    $("#worksheets").text("Hasdfasdfs");
-    const workSheets = tableau.extensions.dashboardContent.dashboard.worksheets;
-    workSheets.forEach(function (worksheet) {
-        $("#worksheets").append(`<button class='btn btn-secondary btn-block'>Hi</button>`);
+    $("#worksheets").text("");
+    tableau.extensions.dashboardContent.dashboard.worksheets.forEach(function (worksheet) {
+        $("#worksheets").append("<button class='btn btn-secondary btn-block'>"+worksheet.name+"</button>");
     });
   }
 })();
