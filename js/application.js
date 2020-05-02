de90f1adc6ebd79d842f797c89a90abbabb703e9
sr10pk16 <pk_prashant@yahoo.com>
2020-05-03 03:25:58 +0530

    050320-0325

--- js/application.js
+++ js/application.js
@@ -2,15 +2,14 @@
  
 (function () {
   $(document).ready(function () {
-
     // Initialises Tableau Data Extension
     tableau.extensions.initializeAsync().then(function () {
-        document.writeln("Prashant");
         refresh();
     }, function () { console.log('Error while Initializing: ' + err.toString()); });
   });
  
   function refresh() {
+      
     // Gets a list of the worksheets and adds them to the web page.
     $("#worksheets").text("");
     tableau.extensions.dashboardContent.dashboard.worksheets.forEach(function (worksheet) {
